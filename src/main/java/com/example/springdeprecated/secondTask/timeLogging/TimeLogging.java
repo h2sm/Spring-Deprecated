01098a310c340600149d23251645c1ec9d802035
Egor Fedorenko <fedore.egor@gmail.com>
2021-10-18 21:57:35 +0300

    upd

--- src/main/java/com/example/springdeprecated/secondTask/timeLogging/TimeLogging.java
+++ src/main/java/com/example/springdeprecated/secondTask/timeLogging/TimeLogging.java
@@ -1,64 +1,37 @@
 package com.example.springdeprecated.secondTask.timeLogging;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.stereotype.Component;
-
 import java.util.*;
 
 @Aspect
 @Slf4j
 public class TimeLogging {//логгируем имя класса + имя метода и его время исполнения
-    private TreeMap<String, Long> map = new TreeMap<>();
+    private static Map<String, Long> map = new TreeMap<>();
 
-    //    @Before("point()")
-//    public void start(JoinPoint jp) {
-//        var nowTime = System.currentTimeMillis();
-//        var classname = jp.getSignature().getDeclaringTypeName();
-//        var methodName = jp.getSignature().getName();
-//        var key = classname + "-" + methodName;
-//        map.put(key, nowTime);
-//    }
-//
-//    @After("point()")
-//    public void stop(JoinPoint jp) {
-//        var classname = jp.getSignature().getDeclaringTypeName();
-//        var methodName = jp.getSignature().getName();
-//        var key = classname + "-" + methodName;
-//        if (map.containsKey(key)) {
-//            var startTime = map.get(key);
-//            var diff = System.currentTimeMillis() - startTime;
-//            map.put(key, diff);
-//        }
-//        print();
-//    }
-//
-//    private void print() {
-//        map.entrySet().stream()
-//                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-//                .forEach(System.out::println);
-//    }
+    @After("point()")
+    private void print() {
+        map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach(System.out::println);
+        System.out.println(map.size());
+    }
     @Around("point()")
-    @SneakyThrows
-    public Object timeCounter(ProceedingJoinPoint pjp) {
+    public Object timeCounter(ProceedingJoinPoint pjp) throws Throwable {
         String targetClass = pjp.getTarget().getClass().getSimpleName();
         String targetMethod = pjp.getSignature().getName();
-        String arg = pjp.getArgs()[0].toString();
-        log.info("Executing {}.{} with argument: {}", targetClass, targetMethod, arg);
-
-        List<String> response = (List<String>) pjp.proceed();
-
-        log.info("Method returned: {}", response);
-
-        return response;
-
+        var key = targetClass + "-" + targetMethod;
+        long startTime = System.currentTimeMillis();
+        Object obj = pjp.proceed();
+        long endtime = System.currentTimeMillis();
+        map.put(key,endtime-startTime);
+        return obj;
     }
 
 
-    @Pointcut("execution(* com.example.springdeprecated.secondTask.somePackage.*.*(..))")
+    //@Pointcut("execution(* com.example.springdeprecated.secondTask.somePackage.*.*(..))")
+    @Pointcut("within(com.example.springdeprecated.secondTask.somePackage..*)")
     public void point() {
 
     }
